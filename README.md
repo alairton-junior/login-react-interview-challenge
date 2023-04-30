e9c5db7c855e4c4e654ffc4cab3e36fd6e2c1cb4
Alairton Junior <alairton20@gmail.com>
2023-04-30 14:08:42 -0300

    Resolução do desafio

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-# fernandev
+# React Interview Challenge
 
-## React Interview Challenge #02
+## Desafio de Entrevista com React
 
-E aí dev, tudo bem? Trouxe pra você mais um desafio de entrevista para que você possa treinar em casa e aprimorar suas habilidades.
+Nesse repositório você encontra a resolução do 2 React Interview Challenge proposto pelo fernadev.
 
-Dessa vez temos um formulário de login incompleto e a sua missão é finalizar ele para a sua equipe.
+O desafio é completar o desenvolvimento de um formulário de login que está incompleto e finalizar ele para a sua equipe.
 
-Preste bem atenção às instruções dentro do arquivo App.jsx e bora codar!
+Instruções:
+* Você tem um formulário de login INCOMPLETO
+* Não é permitido adicionar novos elementos HTML
+* Não é permitido usar refs
 
-**Se você curtir esse conteúdo, deixa uma STAR aí nesse repo,** porque isso me motiva a trazer mais conteúdos desse gênero.
+Tarefas:
+todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.✅
+todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.✅
+todo - Desabilite o botão de Login equanto você está executando o login. ✅
+todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login. ✅
+todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.✅
 
-## Resolução do desafio
+Habilidades 
+useState
+Promises
+Validação de formulários 
+
+
+
+
+Segue o vídeo do desafio e resolução no canal do fernandev
 
 https://youtu.be/NAtf3T9gG7s
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { login } from './utils';
 import './index.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Instruções:
 // * Você tem um formulário de login INCOMPLETO
@@ -8,30 +8,63 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login equanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.✅
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.✅
+// todo - Desabilite o botão de Login equanto você está executando o login. ✅
+// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login. ✅
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.✅
 
 export default function LoginForm() {
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  useEffect(() => {
+    if(email && password.length >= 6) {
+      setIsLogin(false)
+    } else {
+      setIsLogin(true)
+    }
+  }, [email, password])
+
+  function handleLogin() {
+      const user = {email: email, password: password}
+
+      setEmail('')
+      setPassword('')
+      setErrorMessage(null)
+
+      login(user)
+        .then(() => {
+          alert('Login realizado com sucesso.');
+        })
+        .catch((error) => {
+          setErrorMessage(error.message)
+        .finally(() => {
+          setIsLogin(true)
+        })
+    });
+
+  }
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
         <h1>Login Form 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
-        <div className='errorMessage'></div>
+        {errorMessage ? <div className='errorMessage'>{ errorMessage }</div> : null}
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} autoComplete='off' value={email} onChange={(e) => setEmail(e.target.value)}/>
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} value={password} onChange={(e) => setPassword(e.target.value)}/>
         </div>
-
         <div className='button'>
-          <button>Login</button>
+          <button disabled={isLogin} onClick={handleLogin} >Login</button>
         </div>
       </div>
     </div>
